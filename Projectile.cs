b2160519dbf659f56b722c49dc10565480c72f20
StudentUdMir <stududmir@gmail.com>
2026-03-10 11:55:43 +0300

    Update Projectile.cs

--- Projectile.cs
+++ Projectile.cs
@@ -1,23 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Projectile : MonoBehaviour
 {
     public float speed = 10f;
-
+ 
     public GameObject explosion;
-
+ 
     void Start()
     {
         Destroy(gameObject, 1f);
     }
-
+ 
     void Update()
     {
         transform.position = (Vector2)transform.position + Vector2.up * speed * Time.deltaTime;
     }
-
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Obstacle")
@@ -29,3 +29,4 @@ public class Projectile : MonoBehaviour
         }
     }
 }
+
